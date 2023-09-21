1b83d66308d58b688dd15589c3b24b97e6331b8c
Kaze <vunguyen11204@gmail.com>
2023-09-21 18:31:11 +0700

    first pull
    lol

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/papers', title: 'Papers' },
+  { href: '/about', title: 'About' },
+  { href: '/sponsor', title: 'Sponsor' },
+]
+
+export default headerNavLinks
